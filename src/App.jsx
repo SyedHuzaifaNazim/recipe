d28e1aaeb9d477025749f098a88d648c6c0e6fa4
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-08-18 03:51:14 +0500

    building...

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,79 @@
-import { useState,useEffect } from 'react'
-import './App.css'
-import axios from 'axios'
-import Header from '../src/Components/Header'
-import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from '../src/Components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button } from 'react-bootstrap';
 
 function App() {
-  const [myData, setMyData] = useState([]);
-  useEffect(()=>{
+  const [recipes, setRecipes] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
     axios.get('https://dummyjson.com/recipes')
-    .then(response => {
-      setMyData(response.data.recipes)
-    })
-    .catch(error => {
-      console.error(error)
-    })
-  }, [])
-  
-  // const imagePath = require(`./assets/${pngwing.com}.png`);
+      .then(response => {
+        setRecipes(response.data.recipes);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const filteredData = recipes.filter(recipe => recipe.title.toLowerCase().includes(searchQuery.toLowerCase()));
+    setSearchResults(filteredData);
+  };
+
   return (
-    <>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Header />} />
-          </Routes>
-        </Router>
-        <h1 className='recipe-title'>Home Made Oat Flour Pizza</h1>
-        {/* <img src={imagePath} alt="" sizes="" srcset="assets" /> */}
-    </>
-  )
+    <div className="container">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Header />} />
+        </Routes>
+      </Router>
+      <h1 className='recipe-title'>Home Made Oat Flour Pizza</h1>
+      <h1 className='head'>My Recipes</h1>
+      <Form onSubmit={handleSearch}>
+        <Form.Control type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Search recipes" />
+        <Button type="submit">Search</Button>
+      </Form>
+      <ul className="recipe-list">
+        {searchResults.length > 0 ? (
+          searchResults.map((recipe, index) => (
+            <li key={index} className="recipe-header">
+              <img src={recipe.thumbnail} alt={recipe.title} className="recipe-image" />
+              <div className="recipe-info">
+                <h2 className="recipe-title">{recipe.title}</h2>
+                <p>ID: {recipe.id}</p>
+                <ul className="recipe-ingredients-list">
+                  {recipe.ingredients.map((ingredient, ingredientIndex) => (
+                    <li key={ingredientIndex} className="recipe-ingredients-list-item">{ingredient}</li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          ))
+        ) : (
+          recipes.map((recipe, index) => (
+            <li key={index} className="recipe-header">
+              <img src={recipe.thumbnail} alt={recipe.title} className="recipe-image" />
+              <div className="recipe-info">
+                <h2 className="recipe-title">{recipe.title}</h2>
+                <p>ID: {recipe.id}</p>
+                <ul className="recipe-ingredients-list">
+                  {recipe.ingredients.map((ingredient, ingredientIndex) => (
+                    <li key={ingredientIndex} className="recipe-ingredients-list-item">{ingredient}</li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -16,16 +16,18 @@ function Header() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="#link">Recipes</Nav.Link>
+            <NavDropdown title="More" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Contact</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                About
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+              Support Us
+              </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Sign in
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
