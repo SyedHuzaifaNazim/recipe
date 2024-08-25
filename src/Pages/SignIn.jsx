e4457aaf0468479b6b99ac4b017f78af5dd5f65a
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-08-26 00:18:05 +0500

    almost half.... but still : building...

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,65 +1,22 @@
 import React from "react";
 
 function SignIn() {
-    return React.createElement(
-        "div",
-        null,
-        React.createElement(
-            "h1",
-            null,
-            "Sign In"
-            ),
-        React.createElement(
-            "form",
-            null,
-            React.createElement(
-                "label",
-                null,
-                "Email: "
-                ),
-            React.createElement(
-                "input",
-                {
-                    type: "email",
-                    name: "email",
-                    placeholder: "Enter your email"
-                    }
-            ),
-            React.createElement(
-                "br",
-                null
-                ),
-                React.createElement(
-                    "label",
-                    null,
-                    "Password: "
-            ),
-            React.createElement(
-                "input",
-                {
-                    type: "password",
-                    name: "password",
-                    placeholder: "Enter your password"
-                    }
-            ),
-            React.createElement(
-                "button",
-                null,
-                "Sign In"
-            ),
-            React.createElement(
-                "a",
-                {
-                    href: "#",
-                    style: {
-                        color: "#0000ff",
-                        textDecoration: "none"
-                        }
-                        },
-                        "Forgot Password?"
-            ),
-        ),
-    )
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <form>
+        <label>Email: </label>
+        <input type="email" name="email" placeholder="Enter your email" />
+        <br />
+        <label>Password: </label>
+        <input type="password" name="password" placeholder="Enter your password" />
+        <button>Sign In</button>
+        <a href="#" style={{ color: "#0000ff", textDecoration: "none" }}>
+          Forgot Password?
+        </a>
+      </form>
+    </div>
+  );
 }
 
 export default SignIn;
