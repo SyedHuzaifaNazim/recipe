df8727425a2e0e8edc66ba3594ca48b14d147e6e
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-10-02 21:07:37 +0500

    address modified

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,6 +4,16 @@ function Footer() {
     return (
       <footer className="footer bg-white-transparent text-black text-center py-3 mt-6">
         <p>&copy; 2024 MyRecipes App. All rights reserved.</p>
+        <address>
+          <a href="https://www.facebook.com/myrecipesapp" target="_blank" rel="" 
+          className="text-black hover:text-gray-900">facebook</a>
+          <br />
+          <a href="https://www.instagram.com/myrecipesapp" target="_blank" rel=""
+          className="text-black hover:text-gray-900">Instagram</a>
+          <br />
+          <a href="https://www.twitter.com/myrecipesapp" target="_blank" rel=""
+          className="text-black hover:text-gray-900">X</a>
+        </address>
       </footer>
     );
   }
